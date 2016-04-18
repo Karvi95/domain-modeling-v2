9d8791ced41d24046d664d943985d74687a7ba2a
karvi90 <karvi90@uw.edu>
2016-04-18 15:53:55 -0700

    Templates for all 3 tests created

---
+++ SimpleDomainModel/SimpleDomainModelTests/DoubleExtensionTests.swift
@@ -0,0 +1,22 @@
+//
+//  DoubleExtensionTests.swift
+//  SimpleDomainModel
+//
+//  Created by iGuest on 4/18/16.
+//  Copyright © 2016 Ted Neward. All rights reserved.
+//
+
+import XCTest
+
+class DoubleExtensionTests: XCTestCase {
+    func USDDoubleTest() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func YENDoubleTest() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+}
---
+++ SimpleDomainModel/SimpleDomainModelTests/MathematicsTests.swift
@@ -0,0 +1,20 @@
+//
+//  MathematicsTests.swift
+//  SimpleDomainModel
+//
+//  Created by iGuest on 4/18/16.
+//  Copyright © 2016 Ted Neward. All rights reserved.
+//
+
+import XCTest
+
+class MathematicsTests: XCTestCase {
+    func AddingOperatorTest() {
+        
+    }
+
+    func MinusingOperatorTest() {
+        
+    }
+
+}
